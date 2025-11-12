a43de334e8f8ee4b2c92016b2324b0f4771ef232
Reza Ghasemi <rezaghasemi@Rezas-MacBook-Air.local>
2025-11-12 15:29:15 -0700

    data ingestion for local dataset is completed

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,6 +1,7 @@
 from logger import get_logger
 from pathlib import Path
 from config_reader import load_config
+from random import shuffle
 
 logger = get_logger(__name__)
 
@@ -10,7 +11,10 @@ class DataIngention:
         self.config = load_config(config)
         self.train_ratio = self.config["data_ingestion"]["train_ratio"]
         self.test_ratio = self.config["data_ingestion"]["test_ratio"]
-        self.val_ratio = self.config["data_ingestion"]["val_ratio"]
+        self.validation_ratio = self.config["data_ingestion"]["validation_ratio"]
+        self.train_data = None
+        self.test_data = None
+        self.validation_data = None
 
         if self.config["data_ingestion"]["data_source"] == "local":
             logger.info("Data source is local.")
@@ -20,8 +24,8 @@ class DataIngention:
                 raise FileNotFoundError(f"File {file_path} does not exist.")
 
             logger.info(f"Loading data from {self.config['data_ingestion']['source']}")
-            self.raw_data_train, self.raw_data_test, self.raw_data_val = (
-                self.load_loacal_data(self.file_path)
+            self.train_data, self.test_data, self.validation_data = (
+                self.load_loacal_data(file_path)
             )
 
             logger.info("Raw data saved to artifact folder.")
@@ -34,4 +38,26 @@ class DataIngention:
         with open(file_path, "r") as file:
             data = file.read().splitlines()
         num_snapshots = int(data[0])
-        pass
+        row, column = int(data[1].split()[0]), int(data[1].split()[1])
+        data = data[2:]
+        test_data = []
+        output_data = []
+        for t in range(num_snapshots):
+            # t is the time index
+            # j is the row index
+            # i is the column index
+            for j in range(row):
+                line = data[t * row + j].split()
+                for i in range(column):
+                    if int(line[i]) == -1:
+                        test_data.append((t, (j, i), int(line[i])))
+                    else:
+                        output_data.append((t, (j, i), int(line[i])))
+        shuffle(output_data)
+        train_data = output_data[: int(len(output_data) * self.train_ratio)]
+        validation_data = output_data[int(len(output_data) * self.train_ratio) :]
+        return train_data, test_data, validation_data
+
+
+if __name__ == "__main__":
+    data_ingestion = DataIngention(config="src/config/config.yaml")
