eea8cd70d0bf33ad5daa917c88b72288f02947dd
Reza Ghasemi <rezaghasemi@Rezas-MacBook-Air.local>
2025-11-15 04:46:17 -0700

    MLFlow Model tracking is added.

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -5,7 +5,7 @@ from sklearn.metrics import root_mean_squared_error
 from logger import get_logger
 from config_reader import load_config
 import joblib
-
+import mlflow
 
 logger = get_logger(__name__)
 
@@ -28,8 +28,10 @@ class ModelTrainer:
         proccessed_data_path = (
             Path(self.config["data_ingestion"]["artifact_dir"]) / "processed_data"
         )
-        train_data = pd.read_csv(proccessed_data_path / "processed_train_data.csv")
-        val_data = pd.read_csv(proccessed_data_path / "processed_validation_data.csv")
+        self.train_data_path = proccessed_data_path / "processed_train_data.csv"
+        self.val_data_path = proccessed_data_path / "processed_validation_data.csv"
+        train_data = pd.read_csv(self.train_data_path)
+        val_data = pd.read_csv(self.val_data_path)
         return train_data, val_data
 
     def train_model(
@@ -50,24 +52,33 @@ class ModelTrainer:
         return rmse
 
     def run(self):
-        logger.info("Starting model training")
-        train_data, val_data = self.load_data()
-        model = self.create_model()
-        model = self.train_model(model, train_data)
-        rmse = self.evaluate_model(model, val_data)
-        logger.info(f"Model evaluation completed with RMSE: {rmse}")
-        self.save_model(model, rmse)
-        logger.info("Model training completed successfully")
+        mlflow.set_experiment(self.config["model_trainer"]["experiment_name"])
+        with mlflow.start_run():
+            mlflow.set_tag("model", "RandomForestRegressor")
+            logger.info("Starting the model training")
+            train_data, val_data = self.load_data()
+            mlflow.log_artifact(self.train_data_path, "datasets")
+            mlflow.log_artifact(self.val_data_path, "datasets")
+            model = self.create_model()
+            model = self.train_model(model, train_data)
+            rmse = self.evaluate_model(model, val_data)
+            mlflow.log_metric("rmse", rmse)
+            logger.info(f"Model evaluation completed with RMSE: {rmse}")
+            self.save_model(model, rmse)
+            mlflow.log_artifact(self.model_saving_path, "models")
+            model_parameters = model.get_params()
+            mlflow.log_params(model_parameters)
+            logger.info("Model training completed successfully")
 
     def save_model(self, model: RandomForestRegressor, rmse: float):
         logger.info(f"Saving model with RMSE: {rmse}")
-        saving_path = (
+        self.model_saving_path = (
             Path(self.config["model_trainer"]["save_model_path"])
             / f"model_{int(rmse)}.joblib"
         )
-        saving_path.parent.mkdir(parents=True, exist_ok=True)
-        joblib.dump(model, saving_path, compress=3)
-        logger.info(f"Model saved to {saving_path}")
+        self.model_saving_path.parent.mkdir(parents=True, exist_ok=True)
+        joblib.dump(model, self.model_saving_path, compress=3)
+        logger.info(f"Model saved to {self.model_saving_path}")
 
 
 if __name__ == "__main__":
